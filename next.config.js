5e64ce097bd3c58a335c63a8662856c2e1e5a924
Akashnehe <120267501+Akashnehe@users.noreply.github.com>
2025-03-31 20:45:31 +0530

    Restore Next.js configuration to working state

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/website',
   images: {
     unoptimized: true,
   },
@@ -9,18 +8,6 @@ const nextConfig = {
     optimizeCss: true,
   },
   trailingSlash: true,
-  assetPrefix: '/website/',
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(png|jpg|gif|svg|webp)$/i,
-      type: 'asset/resource',
-    });
-    return config;
-  },
-  distDir: 'out',
-  generateBuildId: async () => {
-    return 'build-' + Date.now();
-  },
 }
 
 module.exports = nextConfig 
